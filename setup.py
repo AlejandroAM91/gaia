484c60b25304f08a8163875df5b9a0dc5f734c5a
Alejandro Alonso Mayo <alejandroalonsomayo@gmail.com>
2020-06-06 18:01:06 +0200

    doc: updates metadata

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 AUTHOR = 'Alejandro Alonso Mayo'
 AUTHOR_EMAIL = 'alejandroalonsomayo@gmail.com'
+LICENSE = 'MIT License'
+LICENSE_FILE = 'LICENSE'
 URL = 'https://github.com/AlejandroAM91/gaia'
 DOWNLOAD_URL = f'https://github.com/AlejandroAM91/gaia/archive/v{VERSION}.tar.gz'
 CLASSIFIERS = [
-    "Programming Language :: Python :: 3.8",
-    "License :: OSI Approved :: MIT License",
-],
+    'Programming Language :: Python :: 3.7',
+    'License :: OSI Approved :: MIT License',
+]
 
 with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
+    readme_text = readme_file.read()
+
+with open("LICENSE", "r") as license_file:
+    license_text = license_file.read()
 
 requirements = [
 ]
@@ -21,12 +26,14 @@ setup(
     version = VERSION,
     author = AUTHOR,
     author_email = AUTHOR_EMAIL,
+    license = LICENSE,
+    license_file = LICENSE_FILE,
     description='Project generator',
-    long_description = readme,
+    long_description = readme_text,
     long_description_content_type = 'text/markdown',
     url = URL,
     download_url = DOWNLOAD_URL,
-    classifiers = list(CLASSIFIERS),
+    classifiers = CLASSIFIERS,
     install_requires = requirements + ['jinja2'],
     package_dir={'': 'src'},
     packages=['am91.gaia'],
@@ -40,12 +47,14 @@ setup(
     version = VERSION,
     author = AUTHOR,
     author_email = AUTHOR_EMAIL,
+    license = LICENSE,
+    license_file = LICENSE_FILE,
     description = 'Basic project template for gaia project generator',
-    long_description = readme,
+    long_description = readme_text,
     long_description_content_type = 'text/markdown',
     url = URL,
     download_url = DOWNLOAD_URL,
-    classifiers = list(CLASSIFIERS),
+    classifiers = CLASSIFIERS,
     install_requires = requirements + ['am91-gaia'],
     package_dir = {'': 'src'},
     packages = ['am91.gaia_template_base'],
