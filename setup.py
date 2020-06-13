5055b4c9303a3975e5f468d1011fbc8dfe1adbaa
Alejandro Alonso Mayo <alejandroalonsomayo@gmail.com>
2020-06-13 19:44:39 +0200

    build: updates package format

--- setup.py
+++ setup.py
@@ -1,71 +1,37 @@
 from setuptools import setup
 
-VERSION = '0.0.2'
-AUTHOR = 'Alejandro Alonso Mayo'
-AUTHOR_EMAIL = 'alejandroalonsomayo@gmail.com'
-LICENSE = 'MIT License'
-LICENSE_FILE = 'LICENSE'
-URL = 'https://github.com/AlejandroAM91/gaia'
-DOWNLOAD_URL = f'https://github.com/AlejandroAM91/gaia/archive/v{VERSION}.tar.gz'
-CLASSIFIERS = [
-    'Programming Language :: Python :: 3.7',
-    'License :: OSI Approved :: MIT License',
-]
+VERSION = '0.0.3'
+REQUIREMENTS = ['jinja2']
 
-with open("README.md", "r") as readme_file:
-    readme_text = readme_file.read()
-
-with open("LICENSE", "r") as license_file:
-    license_text = license_file.read()
-
-requirements = [
-]
+with open('README.md', 'r') as readme_file:
+    README = readme_file.read()
 
 setup(
     name='am91-gaia',
     version = VERSION,
-    author = AUTHOR,
-    author_email = AUTHOR_EMAIL,
-    license = LICENSE,
-    license_file = LICENSE_FILE,
+    author = 'Alejandro Alonso Mayo',
+    author_email = 'alejandroalonsomayo@gmail.com',
     description='Project generator',
-    long_description = readme_text,
+    long_description = README,
     long_description_content_type = 'text/markdown',
-    url = URL,
-    download_url = DOWNLOAD_URL,
-    classifiers = CLASSIFIERS,
-    install_requires = requirements + ['jinja2'],
+    url = 'https://github.com/AlejandroAM91/gaia',
+    download_url = f'https://github.com/AlejandroAM91/gaia/archive/v{VERSION}.tar.gz',
+    classifiers = [
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires = REQUIREMENTS,
     package_dir={'': 'src'},
-    packages=['am91.gaia'],
+    packages=['am91.gaia', 'am91.gaia.template.base'],
     entry_points={
         'console_scripts': 'gaia = am91.gaia.cli:main',
-    }
-)
-
-setup(
-    name = 'am91-gaia-template-base',
-    version = VERSION,
-    author = AUTHOR,
-    author_email = AUTHOR_EMAIL,
-    license = LICENSE,
-    license_file = LICENSE_FILE,
-    description = 'Basic project template for gaia project generator',
-    long_description = readme_text,
-    long_description_content_type = 'text/markdown',
-    url = URL,
-    download_url = DOWNLOAD_URL,
-    classifiers = CLASSIFIERS,
-    install_requires = requirements + ['am91-gaia'],
-    package_dir = {'': 'src'},
-    packages = ['am91.gaia_template_base'],
+        'am91.gaia.template': 'base = am91.gaia.template.base',
+    },
     package_data = {
         'am91.gaia_template_base': [
             'templates/*.jinja',
             'templates/.*.jinja',
             'templates/**/*.jinja'
         ]
-    },
-    entry_points={
-        'am91.gaia.template': 'base = am91.gaia_template_base',
-    },
+    }
 )
